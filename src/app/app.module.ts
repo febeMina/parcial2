20ed08b7d759ff43308d4b5f69221f30c2a5baee
febeMina <febemina5@gmail.com>
2023-10-18 19:01:41 -0600

    Parcial2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Componente1Component } from './componente1/componente1.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorPersonalizadoComponent } from './error-personalizado/error-personalizado.component';
+import { Componente2Component } from './componente2/componente2.component';
+import { Componente3Component } from './componente3/componente3.component';
+import { Componente4Component } from './componente4/componente4.component';
+
+
+const appRoutes: Routes=[
+  {path: 'componente1', component: Componente1Component},
+  {path: 'componente2', component: Componente2Component},
+  {path: 'componente3', component: Componente3Component},
+  {path: 'componente4', component: Componente4Component},
+  {path: '**', component: ErrorPersonalizadoComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component,
+    Componente2Component,
+    Componente3Component,
+    Componente4Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
